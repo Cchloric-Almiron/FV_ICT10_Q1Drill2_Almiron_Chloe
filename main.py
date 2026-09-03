f336572bafa145e08dcd092439a7f9775a9da816
Cchloric-Almiron <almiron.ca@obmontessori.edu.ph>
2026-09-03 21:39:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -47,4 +47,34 @@ def dividing_numbers(e):
     quotient = first_number / second_number
 
     display(f'The quotient of {first_number} and {second_number} is {quotient}', target='results')
+
+def floor_dividing_numbers(e):
+
+    document.getElementById('results').innerHTML = "" #clear 
+
+    first_number = float(document.getElementById('num1').value)
+    second_number = float(document.getElementById('num2').value)
+    floordiv = first_number // second_number
+
+    display(f'The floor division of {first_number} and {second_number} is {floordiv}', target='results')
+
+def moduling_numbers(e):
+    
+    document.getElementById('results').innerHTML = "" #clear 
+
+    first_number = float(document.getElementById('num1').value)
+    second_number = float(document.getElementById('num2').value)
+    remainder= first_number % second_number
+
+    display(f' When {first_number} is divided by {second_number}, its remainder is {remainder}', target='results')
+
+def exponential_numbers(e):
+    
+    document.getElementById('results').innerHTML = "" #clear 
+
+    first_number = float(document.getElementById('num1').value)
+    second_number = float(document.getElementById('num2').value)
+    power= first_number ** second_number
+
+    display(f' When {first_number} is raised to the power of {second_number} it results to {power}', target='results')
   
